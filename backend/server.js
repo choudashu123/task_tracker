0ba32e94a13a1d9be7577ebf3aadcdcb524d9d8e
Ashutosh Choudhary <ashutoshchoudhary.job@gmail.com>
2025-05-02 01:04:37 +0530

    resolved cors bug

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,21 @@ const cors = require('cors');
 const app = express();
 
 dotenv.config();
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://task-tracker-xi-pied.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+
 app.use(express.json());
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
 
+// MongoDB Connection & Server Start
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         app.listen(process.env.PORT || 5000, () => console.log('Server running'));
